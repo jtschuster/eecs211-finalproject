25f6b113a32afca6460852de9de2bef2dc5bd18b
saminkumra <40432672+saminkumra@users.noreply.github.com>
2019-03-10 21:23:30 -0500

    Update model.cpp

--- hwfp/src/model.cpp
+++ hwfp/src/model.cpp
@@ -1,3 +1,28 @@
 #include "model.h"
+#include <stdio.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+
+const bool Dictionary::binarySearch(std::string) {
+
+    std::string word;
+    std::ifstream file("words.txt");
+    if (file.is_open()) {
+        std::vector <std::string> dictionary;
+
+        for(size_t i = 0; i < 276643; ++i) {
+            file >> dictionary[i];
+        }
+    }
+
+    return (std::binary_search(dictionary.begin(), dictionary.end(), word));
+
+
+
+
+}
+
 
 
