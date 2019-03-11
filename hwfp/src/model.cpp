45757dbf2a45070e2990f37d44c16a520cd22d93
saminkumra <40432672+saminkumra@users.noreply.github.com>
2019-03-10 23:22:32 -0500

    Update model.cpp

--- hwfp/src/model.cpp
+++ hwfp/src/model.cpp
@@ -19,10 +19,42 @@ const bool Dictionary::binarySearch(std::string) {
 
     return (std::binary_search(dictionary.begin(), dictionary.end(), word));
 
-
-
-
 }
 
+const int value(char letter) {
+    std::map <char, int> Value;
+    Value.insert(std::pair<char, int>('A', 1));
+    Value.insert(std::pair<char, int>('B', 3));
+    Value.insert(std::pair<char, int>('C', 3));
+    Value.insert(std::pair<char, int>('D', 2));
+    Value.insert(std::pair<char, int>('E', 1));
+    Value.insert(std::pair<char, int>('F', 4));
+    Value.insert(std::pair<char, int>('G', 2));
+    Value.insert(std::pair<char, int>('H', 4));
+    Value.insert(std::pair<char, int>('I', 1));
+    Value.insert(std::pair<char, int>('J', 8));
+    Value.insert(std::pair<char, int>('K', 5));
+    Value.insert(std::pair<char, int>('L', 1));
+    Value.insert(std::pair<char, int>('M', 3));
+    Value.insert(std::pair<char, int>('N', 1));
+    Value.insert(std::pair<char, int>('O', 1));
+    Value.insert(std::pair<char, int>('P', 3));
+    Value.insert(std::pair<char, int>('Q', 10));
+    Value.insert(std::pair<char, int>('R', 1));
+    Value.insert(std::pair<char, int>('S', 1));
+    Value.insert(std::pair<char, int>('T', 1));
+    Value.insert(std::pair<char, int>('U', 1));
+    Value.insert(std::pair<char, int>('V', 4));
+    Value.insert(std::pair<char, int>('W', 4));
+    Value.insert(std::pair<char, int>('X', 8));
+    Value.insert(std::pair<char, int>('Y', 4));
+    Value.insert(std::pair<char, int>('Z', 10));
 
-
+    return Value[letter];
+}
+void Bag::randomize() {
+    vector<Tile> Rack;
+    for (size_t i = 0; i < 7; ++i) {
+        Rack[i] = rand() % 7
+    }
+}
